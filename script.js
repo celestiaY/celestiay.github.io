7fc47c1cbace8561a526043b684b8f860589d655
celestiaY <137923093+celestiaY@users.noreply.github.com>
2023-06-27 13:38:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-fetch('dataset_instagram-scraper_2023-06-27_17-09-00-515.json')
-    .then(response => response.json())
-    .then(data => {
-        const jsonContainer = document.getElementById('json-container');
-        jsonContainer.textContent = JSON.stringify(data, null, 4);
-    })
-    .catch(error => console.error('Error:', error));
+const jsonData = [
+    {
+      "id": "3134534560791957970",
+      "type": "Image",
+      "shortCode": "CuAGpNYMJHS",
+      "caption": "— ☆ wts ꒱ ˚⁎\nlino misc pobs\n \n50 - 20 - 20 \n65\n55 - 45 - 65\n45 - 15 \n65\n\n😸: ‎#بيع_سكيز #تبادل_سكيز #بيع_سترايكيدز\n‎‏‎#بيع_ستراي_كيدز #wtsskz #skzwts #wtsstraykids #straykidswts #skzsell",
+      "hashtags": [
+        "بيع_سكيز",
+        "تبادل_سكيز",
+        "بيع_سترايكيدز",
+        "بيع_ستراي_كيدز",
+        "wtsskz",
+        "skzwts",
+        "wtsstraykids",
+        "straykidswts",
+        "skzsell"
+      ],
+      "mentions": [],
+      "url": "https://www.instagram.com/p/CuAGpNYMJHS/",
+      "commentsCount": 0,
+      "firstComment": "",
+      "latestComments": [],
+      "dimensionsHeight": 787,
+      "dimensionsWidth": 1080,
+      "displayUrl": "https://instagram.fprg5-1.fna.fbcdn.net/v/t51.2885-15/356394710_169936806070643_2372808983910962367_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fprg5-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=J9bZBJ4eup4AX8ZRsck&edm=AOUPxh0BAAAA&ccb=7-5&oh=00_AfDHxmWtpMZSxRxAu0c-o3ELS89kKreqGZUB7DbjGovPCA&oe=64A104C0&_nc_sid=9dc660",
+      "images": [],
+      "alt": null,
+      "likesCount": 0,
+      "timestamp": "2023-06-27T17:07:39.000Z",
+      "childPosts": [],
+      "ownerId": "52964780784"
+    }
+];
+
+const jsonContainer = document.getElementById('json-container');
+jsonContainer.textContent = JSON.stringify(jsonData, null, 4);
